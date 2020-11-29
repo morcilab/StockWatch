97810b407b6a639f7f9747d81808b8cf053bded8
Davide Rossi <daviderossi@unibo.it>
2020-11-29 21:09:04 +0100

    UPDATE to JUnit 5

---
+++ test/com/google/gwt/sample/stockwatcher/StockPriceServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.google.gwt.sample.stockwatcher;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StockPriceServiceImplTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
